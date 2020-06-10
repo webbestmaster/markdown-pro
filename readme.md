839bc7ad8defddef607a9f1121b302e45fc84a60
dim <web.best.master@gmail.com>
2020-06-10 14:48:01 +0300

    add readme.md

---
+++ readme.md
@@ -0,0 +1,37 @@
+## Installation:
+
+### Install npm modules:
+```bash
+$ npm i
+```
+
+### Run project:
+```bash
+$ npm start
+```
+
+### Build app:
+```bash
+$ npm run build
+```
+
+### Check types in run time
+Open .babelrc.
+
+```
+"optInOnly": false, // check types in run time
+"optInOnly": true, // DO NOT check types in run time
+```
+
+## Setup environment for Ubuntu
+At first install nodejs (if needed).
+
+### webpack
+```bash
+$ sudo apt-get clean
+$ sudo apt-get update
+$ sudo apt-get install nasm
+$ sudo apt-get install build-essential
+$ sudo apt-get install gcc
+$ sudo apt-get install --no-install-recommends -y gcc make libpng-dev
+```
--- test/initial.js
+++ test/initial.js
@@ -4,12 +4,23 @@ import assert from 'assert';
 
 import {describe, it} from 'mocha';
 
+import {markdown} from '../src/markdown';
+
+
 describe('Array', () => {
-    describe('#indexOf()', () => {
+    describe('indexOf()', () => {
         it('should return -1 when the value is not present', () => {
             const array: Array<number> = [1, 2, 3];
 
             assert(!array.includes(4), '-1');
+        });
+    });
+
+    describe('initial()', () => {
+        it('should return -1 when the value is not present', () => {
+            const mdString = 'you shall not pass!';
+
+            assert(markdown(mdString) === mdString, 'Error');
         });
     });
 });
