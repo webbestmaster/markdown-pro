6b53961be29070c56223ec4219d295bab49eda58
dim <web.best.master@gmail.com>
2020-08-08 13:15:24 +0300

    add support xiaomi browser

--- src/render/render-util.js
+++ src/render/render-util.js
@@ -23,16 +23,23 @@ export function harArrayListOverflow(
     return false;
 }
 
-const tagSelectorRegExpGlobal = /(<\w+[\S\s]*?>)|(<\/\w+?>)|(<\w+[\S\s]*?\/>)/g;
-
-export function getTagIndexList(html: string): Array<PairNumberArrayType> {
+function getMatchIndexList(html: string, regExp: RegExp): Array<PairNumberArrayType> {
     const resultList: Array<PairNumberArrayType> = [];
-    const matchList = [...html.matchAll(tagSelectorRegExpGlobal)];
+
+    const matchList = html.match(regExp);
+
+    if (!matchList) {
+        return [];
+    }
+
+    let currentIndex = 0;
 
     // eslint-disable-next-line no-loops/no-loops
-    for (const matched of matchList) {
-        const start = matched.index;
-        const end = start + matched[0].length - 1;
+    for (const matchedString of matchList) {
+        const start = html.indexOf(matchedString, currentIndex);
+        const end = start + matchedString.length - 1;
+
+        currentIndex = end;
 
         resultList.push([start, end]);
     }
@@ -40,19 +47,14 @@ export function getTagIndexList(html: string): Array<PairNumberArrayType> {
     return resultList;
 }
 
+const tagSelectorRegExpGlobal = /(<\w+[\S\s]*?>)|(<\/\w+?>)|(<\w+[\S\s]*?\/>)/g;
+
+export function getTagIndexList(html: string): Array<PairNumberArrayType> {
+    return getMatchIndexList(html, tagSelectorRegExpGlobal);
+}
+
 const linkSelectorRegExpGlobal = /(<a\s*?>[\S\s]*?<\/a>)|(<a\s[\S\s]*?>[\S\s]*?<\/a>)|(<a\s+[\S\s]*?\/>)/g;
 
 export function getLinkIndexList(html: string): Array<PairNumberArrayType> {
-    const resultList: Array<PairNumberArrayType> = [];
-    const matchList = [...html.matchAll(linkSelectorRegExpGlobal)];
-
-    // eslint-disable-next-line no-loops/no-loops
-    for (const matched of matchList) {
-        const start = matched.index;
-        const end = start + matched[0].length - 1;
-
-        resultList.push([start, end]);
-    }
-
-    return resultList;
+    return getMatchIndexList(html, linkSelectorRegExpGlobal);
 }
