aa38af40addc32849ce1ede6d5a5eb066bfcc416
dim <web.best.master@gmail.com>
2020-10-01 12:51:09 +0300

    remove extra dependence and code

--- src/parser/footnote/footnote-const.js
+++ src/parser/footnote/footnote-const.js
@@ -7,6 +7,6 @@ export const footnoteTypeMap: {[key: FootnoteTypeType]: FootnoteTypeType} = {
     'super': 'super',
 };
 
-export const footnotePrefix = 'fn-';
+// export const footnotePrefix = 'fn-';
 
 export const findFootnoteMarkGlobalRegExp: RegExp = /\S\[\^[^\]]+?]|\S\^\[[^\]]+?]/g;
