d83b179f5ed4bfcbc3e6c3e781c7d2c29da9391e
dim <web.best.master@gmail.com>
2020-10-01 12:55:39 +0300

    remove extra dependence and code

--- webpack/setting/module/rules.js
+++ webpack/setting/module/rules.js
@@ -19,30 +19,6 @@ module.exports.rules = [
         test: fileRegExp,
         use: [
             {
-                loader: 'image-webpack-loader',
-                options: {
-                    mozjpeg: {
-                        quality: 80, // 0..100
-                        progressive: true,
-                    },
-                    // optipng: {
-                    //     optimizationLevel: 7 // 0..7
-                    // },
-                    // pngquant: {
-                    //     quality: '60-80', // 0..100
-                    //     speed: 1 // 1..10
-                    // },
-                    svgo: {}, // no set up needed
-                    gifsicle: {
-                        optimizationLevel: 3, // 1..3
-                    },
-                    webp: {
-                        quality: 75,
-                        method: 6,
-                    },
-                },
-            },
-            {
                 loader: 'base64-inline-loader',
                 query: {
                     limit: 1,
